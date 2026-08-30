e762585059e20b9ec79241b35ad239d333ca1fb5
Johnny Muscovado <milk.lord@gmail.com>
2026-08-30 08:04:32 +0800

    Add shared boss combat app

---
+++ development/combat-tests/combat-boss-app.js
@@ -0,0 +1,15 @@
+import {CombatApp} from './combat-app.js?v=20260829-2138';
+import {BossCombatModel} from './combat-boss-model.js?v=20260830-0748';
+import {evaluatePartyComposition} from './combat-party.js?v=20260829-1915';
+
+export class BossCombatApp extends CombatApp{
+  reset(){
+    this.model=new BossCombatModel(this.scenario);
+    this.partyComposition=evaluatePartyComposition(this.model.units);
+    this.model.partyComposition=this.partyComposition;
+    this.mode='idle';this.selectedUnitId=null;this.selectedSkillId=null;this.skillPage=0;this.skillPageOwnerId=null;
+    this.model.start();this.result.hidden=true;this.skillHud.hidden=true;this.hud.classList.remove('minimal');
+    this.root.querySelectorAll('.compact').forEach(p=>p.classList.remove('open'));
+    this.renderProgress();this.render();this.driveAI();
+  }
+}
