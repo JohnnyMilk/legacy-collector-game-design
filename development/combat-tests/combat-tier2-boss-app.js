7e0afd33e0346cf2b2f0de2e8a701dcfb597556b
Johnny Muscovado <milk.lord@gmail.com>
2026-08-30 13:20:27 +0800

    Sync enemy skill HUD state in Tier 2 boss app

--- development/combat-tests/combat-tier2-boss-app.js
+++ development/combat-tests/combat-tier2-boss-app.js
@@ -1,4 +1,4 @@
-import {CombatApp} from './combat-app.js?v=20260830-1259';
+import {CombatApp} from './combat-app.js?v=20260830-1311';
 import {Tier2BossCombatModel} from './combat-tier2-boss-model.js?v=20260830-1148';
 import {evaluatePartyComposition} from './combat-party.js?v=20260830-0851';
 
@@ -7,7 +7,7 @@ export class Tier2BossCombatApp extends CombatApp{
     this.model=new Tier2BossCombatModel(this.scenario);
     this.partyComposition=evaluatePartyComposition(this.model.units);
     this.model.partyComposition=this.partyComposition;
-    this.mode='idle';this.selectedUnitId=null;this.selectedSkillId=null;this.skillPage=0;this.skillPageOwnerId=null;this.passivePage=0;this.passivePageOwnerId=null;this.progressPage=0;this.potionDirection=null;
+    this.mode='idle';this.selectedUnitId=null;this.selectedSkillId=null;this.skillPage=0;this.skillPageOwnerId=null;this.passivePage=0;this.passivePageOwnerId=null;this.enemyActivePage=0;this.enemyActivePageOwnerId=null;this.progressPage=0;this.potionDirection=null;
     this.model.start();this.result.hidden=true;this.skillHud.hidden=true;this.hud.classList.remove('minimal');
     this.root.querySelectorAll('.compact').forEach(p=>p.classList.remove('open'));
     this.renderProgress();this.render();this.driveAI();
