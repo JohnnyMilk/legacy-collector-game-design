5c7d73c4a41d454e3efc4dec1b195ff5caedd3d6
Johnny Muscovado <milk.lord@gmail.com>
2026-08-30 13:25:01 +0800

    Refresh boss model status logging

--- development/combat-tests/combat-boss-app.js
+++ development/combat-tests/combat-boss-app.js
@@ -1,5 +1,5 @@
 import {CombatApp} from './combat-app.js?v=20260830-1311';
-import {BossCombatModel} from './combat-boss-model.js?v=20260830-0748';
+import {BossCombatModel} from './combat-boss-model.js?v=20260830-1316';
 import {evaluatePartyComposition} from './combat-party.js?v=20260830-0851';
 
 export class BossCombatApp extends CombatApp{
