5f10e032b3f425426a2db0754d2b87f8836f8005
Johnny Muscovado <milk.lord@gmail.com>
2026-08-30 08:59:38 +0800

    Use updated shared combat HUD in boss tests

--- development/combat-tests/combat-boss-app.js
+++ development/combat-tests/combat-boss-app.js
@@ -1,13 +1,13 @@
-import {CombatApp} from './combat-app.js?v=20260829-2138';
+import {CombatApp} from './combat-app.js?v=20260830-0851';
 import {BossCombatModel} from './combat-boss-model.js?v=20260830-0748';
-import {evaluatePartyComposition} from './combat-party.js?v=20260829-1915';
+import {evaluatePartyComposition} from './combat-party.js?v=20260830-0851';
 
 export class BossCombatApp extends CombatApp{
   reset(){
     this.model=new BossCombatModel(this.scenario);
     this.partyComposition=evaluatePartyComposition(this.model.units);
     this.model.partyComposition=this.partyComposition;
-    this.mode='idle';this.selectedUnitId=null;this.selectedSkillId=null;this.skillPage=0;this.skillPageOwnerId=null;
+    this.mode='idle';this.selectedUnitId=null;this.selectedSkillId=null;this.skillPage=0;this.skillPageOwnerId=null;this.passivePage=0;this.passivePageOwnerId=null;
     this.model.start();this.result.hidden=true;this.skillHud.hidden=true;this.hud.classList.remove('minimal');
     this.root.querySelectorAll('.compact').forEach(p=>p.classList.remove('open'));
     this.renderProgress();this.render();this.driveAI();
