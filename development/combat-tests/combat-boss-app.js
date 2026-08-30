db6e80518b58a741bcd6aa5d2e6fbbe1ad5636e1
Johnny Muscovado <milk.lord@gmail.com>
2026-08-30 09:09:01 +0800

    Sync Boss app with paginated combat HUD

--- development/combat-tests/combat-boss-app.js
+++ development/combat-tests/combat-boss-app.js
@@ -1,4 +1,4 @@
-import {CombatApp} from './combat-app.js?v=20260830-0851';
+import {CombatApp} from './combat-app.js?v=20260830-0908';
 import {BossCombatModel} from './combat-boss-model.js?v=20260830-0748';
 import {evaluatePartyComposition} from './combat-party.js?v=20260830-0851';
 
@@ -7,7 +7,7 @@ export class BossCombatApp extends CombatApp{
     this.model=new BossCombatModel(this.scenario);
     this.partyComposition=evaluatePartyComposition(this.model.units);
     this.model.partyComposition=this.partyComposition;
-    this.mode='idle';this.selectedUnitId=null;this.selectedSkillId=null;this.skillPage=0;this.skillPageOwnerId=null;this.passivePage=0;this.passivePageOwnerId=null;
+    this.mode='idle';this.selectedUnitId=null;this.selectedSkillId=null;this.skillPage=0;this.skillPageOwnerId=null;this.passivePage=0;this.passivePageOwnerId=null;this.progressPage=0;
     this.model.start();this.result.hidden=true;this.skillHud.hidden=true;this.hud.classList.remove('minimal');
     this.root.querySelectorAll('.compact').forEach(p=>p.classList.remove('open'));
     this.renderProgress();this.render();this.driveAI();
